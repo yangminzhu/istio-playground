02c843261e3c9284e905a823a52b6a2a4db9feae
Yangmin Zhu <ymzhu@google.com>
2020-11-24 18:13:26 -0800

    update ext-authz

--- ext_authz/server/main.go
+++ ext_authz/server/main.go
@@ -1,3 +1,17 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -6,15 +20,20 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"strings"
 	"sync"
 
-	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"github.com/gogo/googleapis/google/rpc"
 	"golang.org/x/net/context"
 	"google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc"
+)
+
+const (
+	checkHeader  = "x-ext-authz"
+	allowedValue = "allow"
+	resultHeader = "x-ext-authz-result"
 )
 
 var (
@@ -23,22 +42,19 @@ var (
 )
 
 // ExtAuthzServer implements the ext_authz gRPC and HTTP check request API.
-type ExtAuthzServer struct{}
-
-func check(header string) bool {
-	var token string
-	if strings.HasPrefix(header, "Bearer ") {
-		token = strings.TrimPrefix(header, "Bearer ")
-	}
-	return token == "allow"
+type ExtAuthzServer struct {
+	// For test only
+	httpPort chan int
+	grpcPort chan int
 }
 
 // Check implements gRPC check request.
 func (s *ExtAuthzServer) Check(ctx context.Context, request *auth.CheckRequest) (*auth.CheckResponse, error) {
-	log.Printf("gRPC check attributes: %s\n", request.Attributes)
-
-	if check(request.GetAttributes().GetRequest().GetHttp().GetHeaders()["Authorization"]) {
-
+	if allowedValue == request.GetAttributes().GetRequest().GetHttp().GetHeaders()[checkHeader] {
+		log.Printf("[gRPC][allowed]: %s%s with attributes %v\n",
+			request.GetAttributes().GetRequest().GetHttp().GetHost(),
+			request.GetAttributes().GetRequest().GetHttp().GetPath(),
+			request.GetAttributes())
 		return &auth.CheckResponse{
 			// This actually sets the cookie for the upstream request.
 			// It seems gRPC ext_authz doesn't support setting header for downstream response?
@@ -47,40 +63,52 @@ func (s *ExtAuthzServer) Check(ctx context.Context, request *auth.CheckRequest) 
 					Headers: []*core.HeaderValueOption{
 						{
 							Header: &core.HeaderValue{
-								Key: "Set-Cookie",
-								Value: "xt-AuthZ-Custom-Cookie=abcd5678",
+								Key:   resultHeader,
+								Value: "allowed",
 							},
 						},
 					},
 				},
 			},
-			Status: &status.Status{
-				Code: int32(rpc.OK),
-			},
+			Status: &status.Status{Code: int32(rpc.OK)},
 		}, nil
 	}
 
+	log.Printf("[gRPC][ denied]: %s%s with attributes %v\n",
+		request.GetAttributes().GetRequest().GetHttp().GetHost(),
+		request.GetAttributes().GetRequest().GetHttp().GetPath(),
+		request.GetAttributes())
 	return &auth.CheckResponse{
-		Status: &status.Status{
-			Code: int32(rpc.PERMISSION_DENIED),
+		HttpResponse: &auth.CheckResponse_OkResponse{
+			OkResponse: &auth.OkHttpResponse{
+				Headers: []*core.HeaderValueOption{
+					{
+						Header: &core.HeaderValue{
+							Key:   resultHeader,
+							Value: "denied",
+						},
+					},
+				},
+			},
 		},
+		Status: &status.Status{Code: int32(rpc.PERMISSION_DENIED)},
 	}, nil
 }
 
 // ServeHTTP implements the HTTP check request.
-func (s ExtAuthzServer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	log.Printf("%s check %s%s, headers: %s\n", request.Proto, request.Host, request.URL, request.Header)
-
-	if check(request.Header.Get("Authorization")) {
-		// This should set the HTTP header for downstream response.
-		response.Header().Set("Set-Cookie", "Ext-AuthZ-Custom-Cookie=abcd1234")
+func (s *ExtAuthzServer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	if allowedValue == request.Header.Get(checkHeader) {
+		log.Printf("[HTTP][allowed]: %s %s%s with headers: %s\n", request.Method, request.Host, request.URL, request.Header)
+		response.Header().Set(resultHeader, "allowed")
 		response.WriteHeader(http.StatusOK)
 	} else {
+		log.Printf("[HTTP][ denied]: %s %s%s with headers: %s\n", request.Method, request.Host, request.URL, request.Header)
+		response.Header().Set(resultHeader, "denied")
 		response.WriteHeader(http.StatusForbidden)
 	}
 }
 
-func startGRPC(address string, wg *sync.WaitGroup) {
+func (s *ExtAuthzServer) startGRPC(address string, wg *sync.WaitGroup) {
 	defer func() {
 		wg.Done()
 		log.Printf("Stopped gRPC server")
@@ -88,43 +116,51 @@ func startGRPC(address string, wg *sync.WaitGroup) {
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Printf("Failed to start gRPC server: %v", err)
+		log.Fatalf("Failed to start gRPC server: %v", err)
 		return
 	}
+	// Store the port for test only.
+	s.grpcPort <- listener.Addr().(*net.TCPAddr).Port
 
-	s := grpc.NewServer()
-	auth.RegisterAuthorizationServer(s, &ExtAuthzServer{})
+	server := grpc.NewServer()
+	auth.RegisterAuthorizationServer(server, &ExtAuthzServer{})
 
-	log.Printf("Started gRPC server on %s", address)
-	if err := s.Serve(listener); err != nil {
-		log.Printf("Failed to serve gRPC server: %v", err)
+	log.Printf("Starting gRPC server at %s", listener.Addr())
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC server: %v", err)
 		return
 	}
 }
 
-func startHTTP(address string, wg *sync.WaitGroup) {
+func (s *ExtAuthzServer) startHTTP(address string, wg *sync.WaitGroup) {
 	defer func() {
 		wg.Done()
 		log.Printf("Stopped HTTP server")
 	}()
 
-	s := &http.Server{
-		Addr:    address,
-		Handler: ExtAuthzServer{},
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Failed to create HTTP server: %v", err)
 	}
-	log.Printf("Started HTTP server on %s", address)
-	if err := s.ListenAndServe(); err != nil {
-		log.Printf("Failed to start HTTP server: %v", err)
+	// Store the port for test only.
+	s.httpPort <- listener.Addr().(*net.TCPAddr).Port
+
+	log.Printf("Starting HTTP server at %s", listener.Addr())
+	if err := http.Serve(listener, s); err != nil {
+		log.Fatalf("Failed to start HTTP server: %v", err)
 	}
+}
+
+func (s *ExtAuthzServer) run(httpAddr, grpcAddr string) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go s.startGRPC(httpAddr, &wg)
+	go s.startHTTP(grpcAddr, &wg)
+	wg.Wait()
 }
 
 func main() {
 	flag.Parse()
-	log.Printf("Init with grpcPort %s and httpPort %s", *grpcPort, *httpPort)
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go startGRPC(fmt.Sprintf(":%s", *grpcPort), &wg)
-	go startHTTP(fmt.Sprintf(":%s", *httpPort), &wg)
-	wg.Wait()
+	s := &ExtAuthzServer{httpPort: make(chan int, 1), grpcPort: make(chan int, 1)}
+	s.run(fmt.Sprintf(":%s", *httpPort), fmt.Sprintf(":%s", *grpcPort))
 }
